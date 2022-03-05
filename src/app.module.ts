f92e867b4065bcc7231db75ae6ac809086fefe1c
Adem Mohammed <adem164416@gmail.com>
2022-03-05 22:40:14 +0300

    feat: added todo module boilerplate

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { TodoService } from './todo/todo.service'
+import { TodoController } from './todo/todo.controller'
+import { TodoModule } from './todo/todo.module'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService]
+  imports: [TodoModule],
+  controllers: [AppController, TodoController],
+  providers: [AppService, TodoService]
 })
 export class AppModule {}
