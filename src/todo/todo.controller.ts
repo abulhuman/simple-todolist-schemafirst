b1b5c95e9d028c5bf7e8fe2b39bf264cffd5d844
Adem Mohammed <adem164416@gmail.com>
2022-03-06 08:29:23 +0300

    feat: added PUT /todo/:id endpoint

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,4 +1,5 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common'
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common'
+import { Todo, todos, TodoStatus } from 'src/data'
 import { TodoService } from './todo.service'
 
 @Controller('todo')
@@ -16,5 +17,12 @@ export class TodoController {
   @Post()
   createTodo(@Body() body: { title: string }) {
     return this.todoService.createTodo(body)
+  }
+  @Put(':id')
+  updateTodo(
+    @Param('id') id: string,
+    @Body() body: { title: string; status: TodoStatus }
+  ): Todo {
+    return this.todoService.updateTodo({ id, ...body })
   }
 }
